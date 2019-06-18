7bcc6fe354d63973fc3a36644b6147e8c6b16674
Everton Paiva <evertonpaiva@gmail.com>
2019-06-18 14:17:28 -0300

    Reescrevendo métodos HTTP

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -32,17 +32,44 @@ module.exports = (app) => {
     });
 
     app.get('/livros/form', function(req, resp){
-        resp.marko(require('../views/livros/form/form.marko'));
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
+    });
+
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+    
+        livroDao.buscaPorId(id)
+            .then(livro => 
+                resp.marko(
+                    require('../views/livros/form/form.marko'),
+                    { livro: livro }
+                )
+            )
+            .catch(erro => console.log(erro));
+    
     });
 
     app.post('/livros', function(req, resp){
         console.log(req.body);
 
         const livroDao = new LivroDao(db);
-
+        
         livroDao.adiciona(req.body)
-            .then(resp.redirect('/livros'))
-            .catch(erro => console.log(erro));
+        .then(resp.redirect('/livros'))
+        .catch(erro => console.log(erro));
+        
+    });
+
+    app.put('/livros', function(req, resp){
+        console.log(req.body);
+
+        const livroDao = new LivroDao(db);
+        
+        livroDao.atualiza(req.body)
+        .then(resp.redirect('/livros'))
+        .catch(erro => console.log(erro));
+        
     });
 
     app.delete('/livros/:id', function(req, resp){
