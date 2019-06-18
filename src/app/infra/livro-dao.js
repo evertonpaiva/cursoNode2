49db8542e14bc1c2865c9d52c305b7d5841e2549
Everton Paiva <evertonpaiva@gmail.com>
2019-06-18 13:10:38 -0300

    Persistindo o livro no banco de dados e executando redirecionamento

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -4,8 +4,33 @@ class LivroDao {
         this._db = db;
     }
 
-    lista() {
+    adiciona(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO livros (
+                    titulo,
+                    preco,
+                    descricao
+                ) values (?, ?, ?)    
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+    
+                    resolve();
+                } 
+            )
+        });
+    }
 
+    lista() {
         return new Promise ((resolve, reject) => {
             this._db.all(
                 'SELECT * FROM livros',
@@ -17,9 +42,7 @@ class LivroDao {
                 }
             )
         });
-        
-        
-    }
+    }    
 }
 
 module.exports = LivroDao;
